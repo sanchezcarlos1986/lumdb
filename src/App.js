05c5c37a4c3b59824ba9bafa544ae443434be624
Carlos Sánchez <sanchez.carlos.1986@gmail.com>
2018-05-13 13:18:50 -0400

    feat(Class#9) Iterating Movie list from an array

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Movie from './Movie'
+
+const movies = [
+  {
+    id: 1,
+    title: 'Star Wars'
+  },
+  {
+    id: 2,
+    title: 'Rambo'
+  },
+  {
+    id: 3,
+    title: 'Rocky'
+  }
+]
 
 class App extends Component {
   render() {
@@ -8,11 +24,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {movies.map(movie => <Movie key={movie.id} movie={movie} />)}
       </div>
     );
   }
